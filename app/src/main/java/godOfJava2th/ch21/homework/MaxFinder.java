4f45979fad0c61f06e6c2ef0bc2a167fa235d496
= <=>
2021-10-01 08:28:12 +0900

    feat: 21장 직접해봅시다 진행중

---
+++ app/src/main/java/godOfJava2th/ch21/homework/MaxFinder.java
@@ -0,0 +1,4 @@
+package godOfJava2th.ch21.homework;
+
+public class MaxFinder {
+}
---
+++ app/src/test/java/godOfJava2th/ch21/homework/NumberCheckTest.java
@@ -0,0 +1,11 @@
+package godOfJava2th.ch21.homework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxFinderTest {
+
+    @Test
+    void 
+}
