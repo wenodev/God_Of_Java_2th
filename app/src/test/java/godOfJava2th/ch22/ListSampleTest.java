5d82ef6ddd408375f5c1e1798ac389fb6c5a2abd
= <=>
2021-10-03 00:10:06 +0900

    test: test toArray

--- app/src/test/java/godOfJava2th/ch22/ListSampleTest.java
+++ app/src/test/java/godOfJava2th/ch22/ListSampleTest.java
@@ -1,8 +1,10 @@
 package godOfJava2th.ch22;
 
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 class ListSampleTest {
@@ -14,5 +16,16 @@ class ListSampleTest {
 
         System.out.println(list1.get(0));
         System.out.println(list1.get(1));
+    }
+
+    @Test
+    void test_toArray(){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        String[] strArray = list.toArray(new String[0]);
+        Arrays.stream(strArray).forEach(s -> System.out.println(s));
     }
 }
