c118fbcdd3292f4668c736f78c077f2ef4b3839c
= <=>
2021-10-04 20:09:06 +0900

    test: test HashSet

---
+++ app/src/test/java/godOfJava2th/ch23/SetSampleTest.java
@@ -0,0 +1,28 @@
+package godOfJava2th.ch23;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetSampleTest {
+    String[] cars = new String[]{
+            "Tico", "Sonata", "BMW", "Benz",
+            "Lexus", "Mustang", "Grandeure",
+            "The Beetle", "Mini Cooper", "i30",
+            "BMW", "Lexus", "Carnibal", "SM5",
+            "SM7", "SM3", "Tico"
+    };
+
+    @Test
+    void getCarKinds(){
+        Set<String> carSet = new HashSet<>();
+        carSet.addAll(Arrays.asList(cars));
+        Iterator<String> iterator = carSet.iterator();
+        while (iterator.hasNext()){
+            System.out.print(iterator.next() + " ");
+        }
+    }
+
+}
